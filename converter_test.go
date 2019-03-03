82ba665eaf88451f6c4554cec19104534545a8ad
monirz <monir.smith@gmail.com>
2019-03-03 11:24:43 +0600

    fixed wrong conversion for 400

--- converter_test.go
+++ converter_test.go
@@ -5,16 +5,19 @@ import "testing"
 func TestConverter(t *testing.T) {
 
 	numeralTestData := []numeral{
-		0: numeral{1, "I"},
-		1: numeral{2, "II"},
-		2: numeral{3, "III"},
-		3: numeral{4, "IV"},
-		4: numeral{5, "V"},
-		5: numeral{6, "VI"},
-		6: numeral{9, "IX"},
-		7: numeral{10, "X"},
-		8: numeral{20, "XX"},
-		9: numeral{3999, "MMMCMXCIX"},
+		0:  numeral{1, "I"},
+		1:  numeral{2, "II"},
+		2:  numeral{3, "III"},
+		3:  numeral{4, "IV"},
+		4:  numeral{5, "V"},
+		5:  numeral{6, "VI"},
+		6:  numeral{9, "IX"},
+		7:  numeral{10, "X"},
+		8:  numeral{20, "XX"},
+		9:  numeral{100, "C"},
+		10: numeral{400, "CD"},
+		11: numeral{500, "D"},
+		12: numeral{3999, "MMMCMXCIX"},
 	}
 
 	for _, v := range numeralTestData {
